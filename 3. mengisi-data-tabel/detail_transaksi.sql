e9472470932e1576deab10abfafbec17a0553461
firman1703 <mfirman045@gmail.com>
2023-11-15 15:27:14 +0700

    all query

--- 3. mengisi-data-tabel/detail_transaksi.sql
+++ 3. mengisi-data-tabel/detail_transaksi.sql
@@ -1,9 +1,11 @@
-CREATE TABLE detail_transaksi (
-    id_detail_transaksi INT PRIMARY KEY,
-    id_transaksi INT,
-    id_barang INT,
-    jumlah INT,
-    subtotal DECIMAL(10, 2),
-    FOREIGN KEY (id_transaksi) REFERENCES transaksi(id_transaksi),
-    FOREIGN KEY (id_barang) REFERENCES barang(id_barang)
-);
+INSERT INTO detail_transaksi (id_detail_transaksi, id_transaksi, id_barang, jumlah, subtotal) VALUES
+(1, 1, 1, 2, 2000.00),
+(2, 2, 2, 3, 150.00),
+(3, 3, 3, 1, 80.00),
+(4, 4, 4, 1, 20000.00),
+(5, 5, 5, 5, 75.00),
+(6, 6, 6, 2, 20.00),
+(7, 7, 7, 4, 32.00),
+(8, 8, 8, 1, 70.00),
+(9, 9, 9, 3, 15.00),
+(10, 10, 10, 1, 200.00);
